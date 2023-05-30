1261a902f2a1e7c293a17fcb4fe5afc3c7d79116
Pranavhirulkar4 <91416161+Pranavhirulkar4@users.noreply.github.com>
2023-05-30 13:36:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 from flask import Flask, request, render_template
 from jinja2 import escape
 from flask import Flask
-from mysql.connector import mysql.connnector
+import mysql.connnector
 import sklearn
 import pickle
 import streamlit as st
