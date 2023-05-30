d54454e29815b739c84e1644e77dd8cb20ed5170
Pranavhirulkar4 <91416161+Pranavhirulkar4@users.noreply.github.com>
2023-05-30 13:35:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 from flask import Flask, request, render_template
 from jinja2 import escape
 from flask import Flask
-install mysql-connector-python
+install mysql.connector
 import mysql.connnector
 import sklearn
 import pickle
